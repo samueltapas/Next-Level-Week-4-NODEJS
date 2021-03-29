f7fd0d3180274de36ec136d183d4157371b63cb9
Samuel Ferreira <72091823+samueltapas@users.noreply.github.com>
2021-03-29 13:41:38 -0300

    Initial Commit 🚀

---
+++ src/routes.ts
@@ -0,0 +1,20 @@
+import {Router} from 'express';
+import { SendMailController } from './controllers/SendMailController';
+import { SurveysController } from './controllers/SurveysController';
+import { UserController } from './controllers/UserController';
+
+const router = Router();
+
+const userController = new UserController();
+const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
+
+router.post('/users', userController.create);
+
+router.get('/surveys', surveysController.show);
+router.post('/surveys', surveysController.create);
+
+router.post('/sendMail', sendMailController.execute);
+// router.get('/answers?', sendMailController.execute);
+
+export {router}
